5b38ed3e083cfd0b2a9e9e777557036af6e42934
Juan Robledo <heaxenz@Rikos-Air.lan>
2023-09-11 00:49:43 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+          <div className="App">
+            <JokeList />
+          </div>
+        );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <JokeListt />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
